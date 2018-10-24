0c7ca71b7d70461a66256dcc6d4baa09fc5e6590
Alexey Zinkevych <alexzinkevych@yandex.ua>
2018-10-24 23:56:21 +0300

    Test: Added world bounds

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,6 +1,8 @@
 class TestScene extends Phaser.Scene {
   public player: Phaser.GameObjects.Sprite;
   public cursors: any;
+  public map: Phaser.Tilemaps.Tilemap;
+  public speed: number = 5;
 
   constructor() {
     super({
@@ -15,9 +17,9 @@ class TestScene extends Phaser.Scene {
   }
 
   public create() {
-    const map: Phaser.Tilemaps.Tilemap = this.make.tilemap({ key: "map" });
-    const tileset: Phaser.Tilemaps.Tileset = map.addTilesetImage("Desert");
-    const layer: Phaser.Tilemaps.StaticTilemapLayer = map.createStaticLayer(
+    this.map = this.make.tilemap({ key: "map" });
+    const tileset: Phaser.Tilemaps.Tileset = this.map.addTilesetImage("Desert");
+    const layer: Phaser.Tilemaps.StaticTilemapLayer = this.map.createStaticLayer(
       0,
       tileset,
       0,
@@ -27,23 +29,25 @@ class TestScene extends Phaser.Scene {
     this.player = this.add.sprite(100, 100, "player");
     this.cursors = this.input.keyboard.createCursorKeys();
 
-    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
     this.cameras.main.startFollow(this.player, false);
   }
 
   public update(time: number, delta: number) {
-    this.player.angle += 1;
-    if (this.cursors.left.isDown) {
-      this.player.x -= 5;
+    const { heightInPixels, widthInPixels } = this.map;
+    const { height, width, x, y } = this.player;
+    // this.player.angle += 1;
+    if (this.cursors.left.isDown && x > 0) {
+      this.player.x = Math.max(width / 2, x - this.speed);
     }
-    if (this.cursors.right.isDown) {
-      this.player.x += 5;
+    if (this.cursors.right.isDown && x < widthInPixels) {
+      this.player.x = Math.min(widthInPixels - width / 2, x + this.speed);
     }
-    if (this.cursors.down.isDown) {
-      this.player.y += 5;
+    if (this.cursors.down.isDown && y < heightInPixels) {
+      this.player.y = Math.min(heightInPixels - height / 2, y + this.speed);
     }
-    if (this.cursors.up.isDown) {
-      this.player.y -= 5;
+    if (this.cursors.up.isDown && y > 0) {
+      this.player.y = Math.max(height / 2, y - this.speed);
     }
   }
 }
