7fffc6c02130f0b8561483e2363da50dca7fb1d2
amandewatnitrr <amandewatnitrr@gmail.com>
2021-06-08 23:07:40 +0530

    Final update on game

--- src/components/MusicPlaylist.ts
+++ src/components/MusicPlaylist.ts
@@ -13,7 +13,9 @@ export class MusicPlaylist {
 	}
 
 	public play() {
-		this.player.play();
+		window.addEventListener('keydown', this.playOnUserInput);
+		window.addEventListener('click', this.playOnUserInput);
+		window.addEventListener('touchstart', this.playOnUserInput);
 	}
 
 	public pause() {
@@ -24,17 +26,24 @@ export class MusicPlaylist {
 		return this.playlist[this.currentAudio];
 	}
 
+	private playOnUserInput = () => {
+		this.player.play().catch((err) => console.log(err));
+		window.removeEventListener('keydown', this.playOnUserInput);
+		window.removeEventListener('click', this.playOnUserInput);
+		window.removeEventListener('touchstart', this.playOnUserInput);
+	};
+
 	private handleTrackEnded = () => {
 		if (++this.currentAudio === this.playlist.length) this.currentAudio = 0;
 		this.playerSrc.src = this.src;
 		this.player.load();
 		this.play();
-	}
+	};
 
 	private createPlaylist(): string[] {
 		const arr = Array.from(
 			document.querySelectorAll<HTMLLIElement>('#playlist [data-src]')
-		).map(e => e.dataset.src as string);
+		).map((e) => e.dataset.src as string);
 		arr.unshift(this.playerSrc.src);
 		return arr;
 	}
